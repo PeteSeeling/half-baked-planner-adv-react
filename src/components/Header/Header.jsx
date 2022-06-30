7da461704b6033f083a2695abba20bf6e96dffc1
Pete Seeling <peteseeling@gmail.com>
2022-06-30 15:57:56 -0400

    cleaned up code.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import styles from './Header.css';
 
 export default function Header() {
   const { entries } = useEntries();
+  
   return (
     <header className={styles.header}>
       <h2>My Planner</h2>
